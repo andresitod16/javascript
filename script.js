712cf9668b85cb0250a48dde336408055d15a70a
andres <roandres730@gmail.com>
2026-03-25 20:09:55 -0500

    canina

---
+++ script.js
@@ -0,0 +1,13 @@
+let kilometros= Number(prompt ("cuantos kilometros son"));
+let metros= (kilometros*1000);
+alert ("tu resultado en metros es " +metros)
+
+
+metros= Number(prompt("cuantos metros son"))
+kilometros=(kilometros/1000)
+alert("tu resultado en kilometros son "  + kilometros)
+
+
+
+
+
