762c0517f69fe6d7bf6e109219400f5b9af3794c
andres <roandres730@gmail.com>
2026-03-25 21:14:39 -0500

    base

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let grados = Number(prompt("ingrese la cantidad de grados")) ;
-let fahrenheit= (1.8);
-let convertidor =  (grados* 1.8);
-alert ("tus grados en fahrenit son: " + convertidor)
-
-fahrenheit = Number (prompt("ingresa la cantidad de faherenheit "));
- grados = (33.8);
-
-convertidor =  ( fahrenheit * 33.8);
-alert ("tus fahrenit en grados son: " + convertidor)
+let base = Number( prompt(" ingrese su base"));
+let altura = Number(prompt("ingrese su altura"));
+let resultado = (base * altura ** 2 );
+alert  ("tu resultado es " + resultado );
 
 
 
