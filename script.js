0d370453fdfe55a899dc05b476c435e546fb72fb
andres <roandres730@gmail.com>
2026-03-26 18:40:45 -0500

    segundosminutos

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let segundos = Number(prompt("ingrese la cantidad de segundos"));
-let minutos = (60);
-let convertidor = (segundos / 60);
 
-alert("segundos en minutos son: " + convertidor);
+let segundos = Number(prompt("Ingrese la cantidad de segundos"));
+let minutos = segundos / 60;
+
+alert("Los segundos en minutos son: " + minutos);
+
+let min = Number(prompt("Ingrese la cantidad de minutos"));
+let seg = min * 60;
+
+alert("Los minutos en segundos son: " + seg);
