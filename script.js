6c0e184e363d7c9ef602046dc5c9e616f4a3a789
andres <roandres730@gmail.com>
2026-03-26 20:30:15 -0500

    :(

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let milimetros = Number(prompt("Ingrese la cantidad de milímetros"));
-let centimetros = milimetros / 10;
+let personas = Number(prompt("Cuantas personas comieron?"));
+let nombre = prompt("Quien comió mas?");
+let total = 250000;
+let cada_uno = total / personas;
+let resultado = cada_uno * 2
+alert( nombre  + " es quien paga más y debe pagar " + resultado);
 
-alert("Los milímetros en centímetros son: " + centimetros);
+
+
+
+
+
+
