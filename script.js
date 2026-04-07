94bf5e3e37af9b453bbbba36eb6140a8b7e0ddc3
andres <roandres730@gmail.com>
2026-04-06 21:56:16 -0500

    papel

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-//condicionales
-//si el producto cuesta menos de 100 se suma un 10% por costo de envio y si es mayor de  200 un descuento del 25%
-let precio = Number(prompt("ingrese el precio del producto "))
+let options = [" piedra ", " papel ", "tijera" ];
+let azar = Math.floor( Math.random ( )*3 );
+let computer = options [azar]
 
-if (precio <= 100 ) 
-    { precio= precio *1.10;
-    
-} else if(precio >= 200 )
-    {precio= precio *0.75 
-
-    }
-
-console.log ("el total a pagar es de " , precio )
-
-
-
-
-
-
-
-
-
+let jugador =  prompt ("seleciona piedra , papel , tira ")
+if (usuario == computer) {
+    console.log("empate");
+} else 
