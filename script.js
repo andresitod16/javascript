01c482e08744c9424a63a9c940c23f8f23cceb20
andres <roandres730@gmail.com>
2026-04-06 21:16:37 -0500

    descuentos

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let notas = []
-let n1 = prompt("ingrese su primera nota");
-let n2 = prompt("ingrese su segudna nota");
-let n3 = prompt("ingrese su tercera nota");
-let n4 =  prompt("ingrese su cuarta nota");
-let n5 = prompt("ingrese su quinta nota");
+//condicionales
+//si el producto cuesta menos de 100 se suma un 10% por costo de envio y si es mayor de  200 un descuento del 25%
+let precio = Number(prompt("ingrese el precio del producto "))
 
-notas.push(n1);
-notas.push(n2);
-notas.push(n3);
-notas.push(n4);
-notas.push(n5);
+if (precio <= 100 ) 
+    { precio= precio *1.10;
+    
+} else if(precio >= 200 )
+    {precio= precio *0.75 
 
-let promedio = (n1 + n2 + n3 + n4 + n5 ) / 5;
+    }
 
-console.log ("tu promedio es" , notas );
+console.log ("el total a pagar es de " , precio )
+
 
 
 
