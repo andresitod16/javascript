62ecabf85dd3c9ec8ff1ed2a9ba8b41c23d3acf6
andres <roandres730@gmail.com>
2026-04-07 19:09:23 -0500

    papel

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-let options = [" piedra ", " papel ", "tijera" ];
-let azar = Math.floor( Math.random ( )*3 );
-let computer = options [azar]
+let options = ["piedra", "papel", "tijera"];
+let azar = Math.floor(Math.random() * 3);
+let computer = options[azar];
 
-let jugador =  prompt ("seleciona piedra , papel , tira ")
-if (usuario == computer) {
-    console.log("empate");
-} else 
+let jugador = prompt("Selecciona: piedra, papel o tijera");
+
+if (jugador == computer) {
+    console.log("Empate");
+} else if (jugador == "piedra") {
+    if (computer == "tijera") {
+        console.log("Ganaste");
+    } else {
+        console.log("Perdiste");
+    }
+} else if (jugador == "papel") {
+    if (computer == "piedra") {
+        console.log("Ganaste");
+    } else {
+        console.log("Perdiste");
+    }
+} else if (jugador == "tijera") {
+    if (computer == "papel") {
+        console.log("Ganaste");
+    } else {
+        console.log("Perdiste");
+    }
+} else {
+    console.log("Opción inválida");
+}
+
+console.log("Computadora eligió: " + computer);
