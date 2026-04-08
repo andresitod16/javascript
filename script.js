03f41f0a0dfa356a2da38173abb35829831302cf
andres <roandres730@gmail.com>
2026-04-07 20:08:58 -0500

    pares

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-let options = ["piedra", "papel", "tijera"];
-let azar = Math.floor(Math.random() * 3);
-let computer = options[azar];
+//let options = ["piedra", "papel", "tijera"];
+//let azar = Math.floor(Math.random() * 3);
+//let computer = options[azar];
 
-let jugador = prompt("Selecciona: piedra, papel o tijera");
+//let jugador = prompt("Selecciona: piedra, papel o tijera");
 
-if (jugador == computer) {
-    console.log("Empate");
-} else if (jugador == "piedra") {
-    if (computer == "tijera") {
-        console.log("Ganaste");
+//if (jugador == computer) {
+    //console.log("Empate");
+//} else if (jugador == "piedra") {
+    //if (computer == "tijera") {
+     //   console.log("Ganaste");
+   // } else {
+   //     console.log("Perdiste");
+  //  }
+//} else if (jugador == "papel") {
+   // if (computer == "piedra") {
+ //       console.log("Ganaste");
+ //   } else {
+   //     console.log("Perdiste");
+   // }
+//} else if (jugador == "tijera") {
+  //    console.log("Ganaste");
+    //} else {
+     //   console.log("Perdiste");
+  //  }
+//} else {
+  //  console.log("Opción inválida");
+//}
+
+//console.log("Computadora eligió: " + computer);
+
+
+let numeros = [ 2, 5, 7, 12, 0, 17, 22 ];
+let pares = []
+let impares = []
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 == 0) {
+        pares.push(numeros[i]);
+        
     } else {
-        console.log("Perdiste");
+        impares.push(numeros[i]);
     }
-} else if (jugador == "papel") {
-    if (computer == "piedra") {
-        console.log("Ganaste");
-    } else {
-        console.log("Perdiste");
-    }
-} else if (jugador == "tijera") {
-    if (computer == "papel") {
-        console.log("Ganaste");
-    } else {
-        console.log("Perdiste");
-    }
-} else {
-    console.log("Opción inválida");
 }
+console.log(pares);
+console.log(impares);
 
-console.log("Computadora eligió: " + computer);
