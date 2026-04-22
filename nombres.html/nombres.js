e23802422c9ff80de72eaa588d4bd1855c114e25
andres <roandres730@gmail.com>
2026-04-21 21:39:16 -0500

    nombres

---
+++ nombres.html/nombres.js
@@ -0,0 +1,44 @@
+// // nombres = [
+// //  {nombres: "andres ", apellidos: "rodriguez", edad: 19, altura: 171, peso: 67}
+// // ],
+// //  {nombres: "david", apellidos : "carvjal", edad: 19, altura: 171, peso: 67},
+// //   {nombres: "santiago", apellidos : "cumbe", edad: 18, altura: 179, peso: 70} ;
+ 
+//   for (let i = 0; i< nombres.length; i++) {
+//     console.log (nombres[i].nombres );
+// }
+
+// definir una funcion de masa corporal
+let nombres = [
+  {nombres: "andres", apellidos: "rodriguez", edad: 19, altura: 171, peso: 67},
+  {nombres: "david", apellidos: "carvajal", edad: 19, altura: 180, peso: 97},
+  {nombres: "santiago", apellidos: "cumbe", edad: 18, altura: 179, peso: 60}
+];
+
+function calcularimc(peso, altura) {
+  let alturaMetros = altura / 100;
+  return peso / (alturaMetros ** 2);
+}
+
+for (let i = 0; i < nombres.length; i++) {
+  let persona = nombres[i];
+  let imc = calcularimc(persona.peso, persona.altura)
+
+  let condicion ="";
+  if (imc< 18.5) {
+    condicion= "peso inferior al normal"
+  } else if {
+    (imc >= 18.5 ){
+        condicion= "peso normal"
+    } else if (imc>25.9)
+        condicion= "peso superior del normal"
+  } 
+  
+
+
+  console.log(persona.nombres + " " + persona.apellidos + " tiene un imc de " + imc)}
+
+  //condiciones menos  de 18.5 imprimir pesos inferior normal
+  // peso normal de 18.5 -24.9
+  // peso superior del normal 25.0-29.9
+  // sobrepeso  30.0
--- script.js
+++ script.js
@@ -189,71 +189,110 @@
       //  console.log(año + " no es bisiesto");
    // }
 //}
-let btn1 = document.getElementById("btn1");
+//let btn1 = document.getElementById("btn1");
+//
+//btn1.addEventListener("click", () => {
 
-btn1.addEventListener("click", () => {
+  //  alert("boton 1");
+//
+    //let numero = Math.floor((Math.random() * 10) + 1);
+  //  let intentos = 6;
+//
+   // while (intentos > 0) {
+  //      let jugador = prompt("Adivina el número del 1 al 10");
+//
+        //if (jugador == numero) {
+          //  alert("Ganaste");
+         //   break;
+        //} else {
+       //     intentos--;
+      //      alert("Te quedan: " + intentos);
+    //    }
+  //  }
+//
+    //if (intentos === 0) {
+    //    alert("Perdiste. El número era: " + numero);
+  //  }
+//
+//});
 
-    alert("boton 1");
+//let btn2 = document.getElementById("btn2");
+//
+//btn2.addEventListener("click", () => {
+//
+    //let options = ["piedra", "papel", "tijera"];
+    //let azar = Math.floor(Math.random() * 3);
+  //  let computer = options[azar];
+//
+    //let jugador = prompt("selecciona: piedra, papel o tijera");
+    //if (jugador == computer) {
+      //  alert("empate");
+    //} else if (jugador == "piedra") {
+        //if (computer == "tijera") {
+          //  alert("ganaste");
+          //   } else {
+        //    alert("perdiste");
+      //  }
+    //} else if (jugador == "papel") {
+        //if (computer == "piedra") {
+         //   alert("ganaste");
+        //} else {
+        //    alert("perdiste");
+      //  }
+    //} else if (jugador == "tijera") {
+        //if (computer == "papel") {
+         //   alert("ganaste");
+        //} else {
+       //     alert("perdiste");
+      //  }
+    //} else {
+    //    alert("opcion invalida");
+  //  }
+//
+  //  alert("la computadora eligio: " + computer);
+//
+//});
 
-    let numero = Math.floor((Math.random() * 10) + 1);
-    let intentos = 6;
+//let btn3 = document.getElementById ("btn3");
+//btn3. addEventListener("click ", () => {
+ // /alert ("boton 3")
+//})
 
-    while (intentos > 0) {
-        let jugador = prompt("Adivina el número del 1 al 10");
+const resident = [
+  { nombre: "Leon S. Kennedy", imagen: "https://picsum.photos/200/300?random=1" },
+  { nombre: "Claire Redfield", imagen: "https://picsum.photos/200/300?random=2" },
+  { nombre: "Chris Redfield", imagen: "https://picsum.photos/200/300?random=3" },
+  { nombre: "Jill Valentine", imagen: "https://picsum.photos/200/300?random=4" },
+  { nombre: "Ada Wong", imagen: "https://picsum.photos/200/300?random=5" },
+  { nombre: "Albert Wesker", imagen: "https://picsum.photos/200/300?random=6" },
+  { nombre: "Ethan Winters", imagen: "https://picsum.photos/200/300?random=7" },
+  { nombre: "Rebecca Chambers", imagen: "https://picsum.photos/200/300?random=8" },
+  { nombre: "Carlos Oliveira", imagen: "https://picsum.photos/200/300?random=9" },
+  { nombre: "Sheva Alomar", imagen: "https://picsum.photos/200/300?random=10" }
+];
 
-        if (jugador == numero) {
-            alert("Ganaste");
-            break;
-        } else {
-            intentos--;
-            alert("Te quedan: " + intentos);
-        }
-    }
+document.addEventListener("DOMContentLoaded", () => {
 
-    if (intentos === 0) {
-        alert("Perdiste. El número era: " + numero);
-    }
+  let title = document.querySelector("h3");
+  let contenedor = document.querySelector("section");
+
+  title.textContent = "Personajes Resident Evil";
+
+  for (let i = 0; i < resident.length; i++) {
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let img = document.createElement("img");
+    img.src = resident[i].imagen;
+
+    let nombre = document.createElement("h2");
+    nombre.textContent = resident[i].nombre;
+
+    card.appendChild(img);
+    card.appendChild(nombre);
+
+    contenedor.appendChild(card);
+  }
 
 });
-
-let btn2 = document.getElementById("btn2");
-
-btn2.addEventListener("click", () => {
-
-    let options = ["piedra", "papel", "tijera"];
-    let azar = Math.floor(Math.random() * 3);
-    let computer = options[azar];
-
-    let jugador = prompt("selecciona: piedra, papel o tijera");
-    if (jugador == computer) {
-        alert("empate");
-    } else if (jugador == "piedra") {
-        if (computer == "tijera") {
-            alert("ganaste");
-             } else {
-            alert("perdiste");
-        }
-    } else if (jugador == "papel") {
-        if (computer == "piedra") {
-            alert("ganaste");
-        } else {
-            alert("perdiste");
-        }
-    } else if (jugador == "tijera") {
-        if (computer == "papel") {
-            alert("ganaste");
-        } else {
-            alert("perdiste");
-        }
-    } else {
-        alert("opcion invalida");
-    }
-
-    alert("la computadora eligio: " + computer);
-
-});
-
-let btn3 = document.getElementById ("btn3");
-btn3. addEventListener("click ", () => {
-  alert ("boton 3")
-})
