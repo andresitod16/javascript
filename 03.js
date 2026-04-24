021fc883878fccb95d62236d9de47cd176804dc6
andres <roandres730@gmail.com>
2026-04-23 21:24:42 -0500

    03porterminar

--- 03.js
+++ 03.js
@@ -15,10 +15,28 @@
 // - Archivo `app.js`: Importar los productos y las funciones para mostrar en consola un reporte detallado: `"El producto [Nombre] cuesta $[PrecioTotal] y [Requiere/No requiere] reposición"`.
 
 
-let objetos = [ {producto: "teclado" , precio : "150000" , stock: "6 stock " , categoria: "perifericos"},
-    {producto: "lapto" , precio : "250000" , stock: "2 stock " , categoria: "laptos"},
-    {producto: "sillas" , precio : "950000" , stock: "8 stock " , categoria: "muebles"},
-    {producto: "monitor" , precio : "780000" , stock: " 3 stock " , categoria: "electrodomestico"},
+let objetos = [
+  { producto: "teclado", precio: 150000, stock: "6 stock", categoria: "perifericos" },
+  { producto: "lapto", precio: 250000, stock: "2 stock", categoria: "laptos" },
+  { producto: "sillas", precio: 950000, stock: "8 stock", categoria: "muebles" },
+  { producto: "monitor", precio: 780000, stock: "3 stock", categoria: "electrodomestico" },
 ];
-let iva = 0.19; 
-let precio = Number(objetos.precio)
+
+let iva = 0.19;
+
+for (let i = 0; i < objetos.length; i++) {
+  let producto = objetos[i].producto;
+  let precio = objetos[i].precio;
+  let impuestos = precio * iva;
+  let precio_final = precio + impuestos;
+
+  if(stock< 5)
+    estado = "necesita reposicion";
+else(
+    estado = "no necesita reposicion"
+)
+
+  console.log(
+    `Producto: ${producto}  Precio base: ${precio} IVA: ${impuestos} Precio final: ${precio_final}`
+  );
+}
